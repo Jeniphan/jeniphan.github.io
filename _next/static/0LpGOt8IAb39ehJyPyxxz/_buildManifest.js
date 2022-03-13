32f882c5a51a2f79e58001bd22c76ff3345b950a
Jetniphan Pukkham <jeni8399@gmail.com>
2022-03-13 22:19:10 +0700

    🚀 Deploy [13/03/22]

---
+++ _next/static/0LpGOt8IAb39ehJyPyxxz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6153d949f7816688.css","static/chunks/pages/index-58943f1d110f2a5d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
