a2ac4420c8bb2690ab4421e454992e2d5ec98ed5
Jetniphan Pukkham <jeni8399@gmail.com>
2022-03-20 16:29:42 +0700

    🚀 Deploy [20/03/22]

---
+++ _next/static/-9oottDANMzIOpm36TA1Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4bc674c7f3544bb8.css","static/chunks/pages/index-026f0d8608123197.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/igThfnXula-tR4MX-uCV5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2b44cc70d1307abe.css","static/chunks/pages/index-691c019b98b1b40f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
